40c9520d708d6b6399f2b2b1c69c6198dc78a9e3
hulufei <f.calabash@gmail.com>
2014-11-28 08:45:10 +0000

    Listen for proxy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.npm_package_config_port || 3000;
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true
-}).listen(port, 'localhost', function (err, result) {
+}).listen(port, '0.0.0.0', function (err, result) {
   if (err) {
     console.log(err);
   }
